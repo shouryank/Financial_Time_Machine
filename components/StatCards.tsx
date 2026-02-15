2b6530f8dcb44a95600e4fa27881f3f8e9567643
rrgundam <rrgundam@ncsu.edu>
2026-02-14 20:24:25 -0500

    Changes required run it locally

--- components/StatCards.tsx
+++ components/StatCards.tsx
@@ -136,7 +136,7 @@ const StatCards: React.FC<StatCardsProps> = ({ branch, originalBranch }) => {
             ) : (
               branchAssetItems.map((asset, idx) => (
                 <p key={idx} className="text-cyan-300">
-                  {asset.label}: {formatCurrency(asset.purchaseAmount)} -> {formatCurrency(asset.currentValue)}
+                  {asset.label}: {formatCurrency(asset.purchaseAmount)} {'\u2192'} {formatCurrency(asset.currentValue)}
                 </p>
               ))
             )}
